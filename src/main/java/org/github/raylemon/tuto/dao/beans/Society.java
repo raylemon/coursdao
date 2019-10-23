d0fc882bac0cdafdc1e7058a3dfdc0f26152315f
Ray Lemon <big0424@gmail.com>
2019-10-23 15:36:03 +0200

    Society done

---
+++ src/main/java/org/github/raylemon/tuto/dao/beans/Society.java
@@ -0,0 +1,50 @@
+package org.github.raylemon.tuto.dao.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Society {
+    private long id;
+    private String name;
+    private List<Employee> employees = new ArrayList<>();
+
+    public Society(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public boolean addEmployee(Employee employee) {
+        return employees.add(employee);
+    }
+
+    public boolean removeEmployee(Employee employee) {
+        return employees.remove(employee);
+    }
+
+    public boolean containsEmployee(Employee employee) {
+        return employees.contains(employee);
+    }
+}
