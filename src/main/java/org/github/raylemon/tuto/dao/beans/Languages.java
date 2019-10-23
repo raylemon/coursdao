e5ccc48e794dbd97711f0c4c7a88c344e1cf190b
Ray Lemon <big0424@gmail.com>
2019-10-23 15:28:22 +0200

    Languages done

---
+++ src/main/java/org/github/raylemon/tuto/dao/beans/Languages.java
@@ -0,0 +1,41 @@
+package org.github.raylemon.tuto.dao.beans;
+
+import java.util.Objects;
+
+public class Languages {
+    private long id;
+    private String name;
+
+    public Languages(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Languages languages = (Languages) o;
+        return id == languages.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
